253b0e4e6b760ff261f7ab8a12f5b5ffb80559ad
FelipeAndresBascunanMorales <f.bascunan03@gmail.com>
2025-06-25 10:50:44 -0400

    feat: add scaffolded views and tests with a basic implementation
    just for evaluate everything is wired up and working

---
+++ app/views/bets/_bet.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! bet, :id, :player_id, :round_id, :amount, :color, :created_at, :updated_at
+json.url bet_url(bet, format: :json)
---
+++ test/jobs/play_roulette_round_job_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class PlayRouletteRoundJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
